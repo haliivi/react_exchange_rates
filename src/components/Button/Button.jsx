87a4852ed189dace9775b4a36137a330cde6bd95
a.sotnichenko <haliivi@mail.ru>
2023-08-03 15:48:55 +0700

    1) Fill Sample page. 2) Edit Button component.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ export const Button = props => {
     return (
         <button
             className="btn"
-            onClick={() => props.onClick(props.arg || '') || undefined}
+            onClick={() => props.onClick ? props.onClick(props.arg || '') || undefined : undefined}
         >
             {props.text}
         </button>
--- src/pages/Sample/Sample.jsx
+++ src/pages/Sample/Sample.jsx
@@ -1,7 +1,37 @@
-import './Sample.scss'
+import './Sample.scss';
+import { useContext } from 'react';
+import { RateContext } from '../../context/RateContext';
+import { Button } from '../../components/Button/Button'
 
 export const Sample = () => {
+    const {state} = useContext(RateContext)
     return (
-        <h1>Sample</h1>
+        <div className="sample">
+            <div className="sample-container">
+                <div className="">
+                    <h3>
+                        Получить курс: &nbsp;
+                        <select>
+                            {Object.keys(state.currency).map((item, i) => (
+                                <option key={item}>{item}</option>
+                            ))}
+                        </select>
+                            &nbsp;&nbsp;к&nbsp;&nbsp;
+                        <select>
+                            {Object.keys(state.currency).map((item, i) => (
+                                <option key={item}>{item}</option>
+                            ))}
+                        </select>
+                    </h3>
+                </div>
+                <div className="sample-head">
+                    <span>Дата: <input type="date" /></span>
+                    <Button text='Получить курс' />
+                </div>
+                <div className="sample-result">
+                    <ul></ul>
+                </div>
+            </div>
+        </div>
     )
 }
